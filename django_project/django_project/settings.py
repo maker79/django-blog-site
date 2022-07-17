b8edc22a95ff110d590749cf39665dc9452fff3d
maker79 <vladan.djurkovic@gmail.com>
2022-07-17 14:52:29 -0500

    working on login

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -134,7 +134,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_PASSWORD = os.environ.get('d')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
